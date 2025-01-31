69424b8afa0046eb4a0562c700953ce494780856
pavelkrolevets <pavelkrolevets@gmail.com>
2025-01-31 12:44:17 +0300

    update tests

--- contracts/solana/tests/leea-aico-test.ts
+++ contracts/solana/tests/leea-aico-test.ts
@@ -6,6 +6,7 @@ import { Metaplex } from "@metaplex-foundation/js";
 import { getMint, getAssociatedTokenAddressSync } from "@solana/spl-token";
 import type { LeeaTokenAico } from "../target/types/leea_token_aico";
 import { print_address, print_thread, print_tx, stream_program_logs } from "./utils";
+import assert from "assert";
 
 describe("leea-aico", async () => {
   // Configure the client to use the local cluster.
@@ -21,7 +22,7 @@ describe("leea-aico", async () => {
   );
 
   const ADMIN_PUB_KEY = new web3.PublicKey(
-    "3M6Tp9boFAGWqM9FFeu6QTsgnsobe7UX79aryVtcAWpd"
+    "GB9XNqUC32ZibLza8d7qMKBEv1hPZ142hzZ3sju7hG7b"
   );
   // metaplex setup
   const metaplex = Metaplex.make(provider.connection);
@@ -70,23 +71,14 @@ describe("leea-aico", async () => {
     console.log(`Use 'solana confirm -v ${txHash}' to see the logs`);
   }
 
-  async function fetchAccountData() {
-    const [agentBalance, agentData] = await Promise.all([
-      provider.connection.getTokenAccountBalance(agentTokenAccount),
-      program.account.agentAccount.fetch(agentPDA),
-    ]);
-
-    console.log("Agent Token Balance: ", agentBalance.value.uiAmount);
-    console.log("Agent Name: ", agentData.agentName);
-  }
-
   let txHash;
 
   it("Mint Leea Token", async () => {
     try {
       const mintData = await getMint(provider.connection, rewardTokenMintPDA);
-      console.log("Mint Already Exists");
-    } catch {
+      // Assertions
+      assert.equal(mintData.supply, 0);
+    } catch (e) {
       txHash = await program.methods
         .createMint(metadata.uri, metadata.name, metadata.symbol)
         .accounts({
@@ -96,8 +88,11 @@ describe("leea-aico", async () => {
         })
         .rpc();
       await logTransaction(txHash);
+      console.log("Token Minted: ", rewardTokenMintPDA.toString());
+      const mintData = await getMint(provider.connection, rewardTokenMintPDA);
+      // Assertions
+      assert.equal(mintData.supply, 0);
     }
-    console.log("Token Mint: ", rewardTokenMintPDA.toString());
   });
 
   it("Register Agent", async () => {
@@ -105,56 +100,59 @@ describe("leea-aico", async () => {
       const agentData = await program.account.agentAccount.fetch(agentPDA);
       console.log("Agent Already Exists");
       console.log("Agent: ", agentData.holder.toString());
+      assert.equal(agentData.holder.toString(), provider.wallet.publicKey.toString())
     } catch (e) {
-      console.log("error", e);
       // probably not exist try to create
-      try {
-        txHash = await program.methods
-          .initializeAgent("GPT4")
-          .accounts({
-            agentAccount: agentPDA,
-          })
-          .rpc();
-        await logTransaction(txHash);
-        console.log("Agent Account Created");
-      } catch (e) {
-        console.log("error", e);
-      }
+      txHash = await program.methods
+        .initializeAgent("GPT4")
+        .accounts({
+          agentAccount: agentPDA,
+        })
+        .rpc();
+      await logTransaction(txHash);
+      console.log("Agent Account Created");
+      const agentData = await program.account.agentAccount.fetch(agentPDA);
+      console.log("Agent: ", agentData.holder.toString());
+      assert.equal(agentData.agentName, "GPT4")
+      assert.equal(agentData.holder.toString(), provider.wallet.publicKey.toString())
     }
   });
 
   it("Deposit to agent account for aiCO", async () => {
-    try {
-      txHash = await program.methods
-        .deposit(new BN(10000000))
-        .accounts({
-          recipient: ADMIN_PUB_KEY,
-          agentAccount: agentPDA,
-        })
-        .rpc();
-      await logTransaction(txHash);
-      const agentData = await program.account.agentAccount.fetch(agentPDA);
-      console.log("Agent Balance: ", agentData.balance.toString());
-    } catch (e) {
-      console.log("error", e);
-    }
+    txHash = await program.methods
+      .deposit(new BN(10000000))
+      .accounts({
+        recipient: ADMIN_PUB_KEY,
+        agentAccount: agentPDA,
+      })
+      .rpc();
+    await logTransaction(txHash);
+    const agentData = await program.account.agentAccount.fetch(agentPDA);
+    console.log("Agent Balance: ", agentData.balance.toString());
+    assert.equal(agentData.agentName, "GPT4")
+    assert.equal(agentData.holder.toString(), provider.wallet.publicKey.toString())
   });
 
   it("Mint Leea tokens at aiCO time", async () => {
-    try {
-      txHash = await program.methods
-        .aicoToAgent()
-        .accounts({
-          rewardTokenMint: rewardTokenMintPDA,
-          agentTokenAccount: agentTokenAccount,
-          agentAccount: agentPDA,
-        })
-        .rpc();
-      await logTransaction(txHash);
-      await fetchAccountData();
-    } catch (e) {
-      console.log("error", e);
-    }
+    txHash = await program.methods
+      .aicoToAgent()
+      .accounts({
+        rewardTokenMint: rewardTokenMintPDA,
+        agentTokenAccount: agentTokenAccount,
+        agentAccount: agentPDA,
+      })
+      .rpc();
+    await logTransaction(txHash);
+    const [agentBalance, agentData] = await Promise.all([
+      provider.connection.getTokenAccountBalance(agentTokenAccount),
+      program.account.agentAccount.fetch(agentPDA),
+    ]);
+    console.log("Agent Token Balance: ", agentBalance.value.uiAmount);
+    console.log("Agent Name: ", agentData.agentName);
+    assert.equal(agentData.agentName, "GPT4");
+    assert.equal(agentData.holder.toString(), provider.wallet.publicKey.toString())
+    assert.notEqual(agentBalance.value.uiAmount, 0);
+    assert.equal(agentData.balance, 0);
   });
 });
 
