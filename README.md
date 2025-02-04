f5690a44ccc5014c8b345785e1d227b0bcc9a5c0
pavelkrolevets <pavelkrolevets@gmail.com>
2025-02-04 06:30:27 +0200

    move lib to node_modules + add compiled artifacts

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 ### Leea Smart Contracts (Decentralized core)
+
+
+##### Project Objects:
+* Governance (DAO) 
+* Leea Token
+* Escrow
+* Agent Registry 
+* Validator Registry 
+* Validator Staking
+
--- contracts/LeeaGovernance.sol
+++ contracts/LeeaGovernance.sol
@@ -2,59 +2,38 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.22;
 
-import {GovernorUpgradeable} from "./lib/openzeppelin/contracts-upgradable/governance/GovernorUpgradeable.sol";
-import {GovernorCountingSimpleUpgradeable} from "./lib/openzeppelin/contracts-upgradable/governance/extensions/GovernorCountingSimpleUpgradeable.sol";
-import {GovernorSettingsUpgradeable} from "./lib/openzeppelin/contracts-upgradable/governance/extensions/GovernorSettingsUpgradeable.sol";
-import {GovernorStorageUpgradeable} from "./lib/openzeppelin/contracts-upgradable/governance/extensions/GovernorStorageUpgradeable.sol";
-import {GovernorTimelockControlUpgradeable} from "./lib/openzeppelin/contracts-upgradable/governance/extensions/GovernorTimelockControlUpgradeable.sol";
-import {GovernorVotesUpgradeable} from "./lib/openzeppelin/contracts-upgradable/governance/extensions/GovernorVotesUpgradeable.sol";
-import {GovernorVotesQuorumFractionUpgradeable} from "./lib/openzeppelin/contracts-upgradable/governance/extensions/GovernorVotesQuorumFractionUpgradeable.sol";
-import {Initializable} from "./lib/openzeppelin/contracts/proxy/utils/Initializable.sol";
-import {TimelockControllerUpgradeable} from "./lib/openzeppelin/contracts-upgradable/governance/TimelockControllerUpgradeable.sol";
+import {Governor} from "@openzeppelin/contracts/governance/Governor.sol";
+import {GovernorCountingSimple} from "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import {GovernorStorage} from "@openzeppelin/contracts/governance/extensions/GovernorStorage.sol";
+import {GovernorTimelockControl} from "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
+import {GovernorVotes} from "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import {GovernorVotesQuorumFraction} from "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+import {IVotes} from "@openzeppelin/contracts/governance/utils/IVotes.sol";
+import {TimelockController} from "@openzeppelin/contracts/governance/TimelockController.sol";
 
 /// @custom:security-contact contract_security@leealabs.com
-contract LeeaGovernance is Initializable, GovernorUpgradeable, GovernorSettingsUpgradeable, GovernorCountingSimpleUpgradeable, GovernorStorageUpgradeable, GovernorVotesUpgradeable, GovernorVotesQuorumFractionUpgradeable, GovernorTimelockControlUpgradeable {
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
+contract LeeaGovernance is Governor, GovernorCountingSimple, GovernorStorage, GovernorVotes, GovernorVotesQuorumFraction, GovernorTimelockControl {
+    constructor(IVotes _token, TimelockController _timelock)
+        Governor("LeeaGovernance")
+        GovernorVotes(_token)
+        GovernorVotesQuorumFraction(4)
+        GovernorTimelockControl(_timelock)
+    {}
+
+    function votingDelay() public pure override returns (uint256) {
+        return 1 days;
     }
 
-    function initialize(IVotes _token, TimelockControllerUpgradeable _timelock)
-        initializer public
-    {
-        __Governor_init("LeeaGovernance");
-        __GovernorSettings_init(1 days, 1 weeks, 3e18);
-        __GovernorCountingSimple_init();
-        __GovernorStorage_init();
-        __GovernorVotes_init(_token);
-        __GovernorVotesQuorumFraction_init(4);
-        __GovernorTimelockControl_init(_timelock);
+    function votingPeriod() public pure override returns (uint256) {
+        return 1 weeks;
     }
 
     // The following functions are overrides required by Solidity.
 
-    function votingDelay()
-        public
-        view
-        override(GovernorUpgradeable, GovernorSettingsUpgradeable)
-        returns (uint256)
-    {
-        return super.votingDelay();
-    }
-
-    function votingPeriod()
-        public
-        view
-        override(GovernorUpgradeable, GovernorSettingsUpgradeable)
-        returns (uint256)
-    {
-        return super.votingPeriod();
-    }
-
     function quorum(uint256 blockNumber)
         public
         view
-        override(GovernorUpgradeable, GovernorVotesQuorumFractionUpgradeable)
+        override(Governor, GovernorVotesQuorumFraction)
         returns (uint256)
     {
         return super.quorum(blockNumber);
@@ -63,7 +42,7 @@ contract LeeaGovernance is Initializable, GovernorUpgradeable, GovernorSettingsU
     function state(uint256 proposalId)
         public
         view
-        override(GovernorUpgradeable, GovernorTimelockControlUpgradeable)
+        override(Governor, GovernorTimelockControl)
         returns (ProposalState)
     {
         return super.state(proposalId);
@@ -72,24 +51,15 @@ contract LeeaGovernance is Initializable, GovernorUpgradeable, GovernorSettingsU
     function proposalNeedsQueuing(uint256 proposalId)
         public
         view
-        override(GovernorUpgradeable, GovernorTimelockControlUpgradeable)
+        override(Governor, GovernorTimelockControl)
         returns (bool)
     {
         return super.proposalNeedsQueuing(proposalId);
     }
 
-    function proposalThreshold()
-        public
-        view
-        override(GovernorUpgradeable, GovernorSettingsUpgradeable)
-        returns (uint256)
-    {
-        return super.proposalThreshold();
-    }
-
     function _propose(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, string memory description, address proposer)
         internal
-        override(GovernorUpgradeable, GovernorStorageUpgradeable)
+        override(Governor, GovernorStorage)
         returns (uint256)
     {
         return super._propose(targets, values, calldatas, description, proposer);
@@ -97,7 +67,7 @@ contract LeeaGovernance is Initializable, GovernorUpgradeable, GovernorSettingsU
 
     function _queueOperations(uint256 proposalId, address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
         internal
-        override(GovernorUpgradeable, GovernorTimelockControlUpgradeable)
+        override(Governor, GovernorTimelockControl)
         returns (uint48)
     {
         return super._queueOperations(proposalId, targets, values, calldatas, descriptionHash);
@@ -105,14 +75,14 @@ contract LeeaGovernance is Initializable, GovernorUpgradeable, GovernorSettingsU
 
     function _executeOperations(uint256 proposalId, address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
         internal
-        override(GovernorUpgradeable, GovernorTimelockControlUpgradeable)
+        override(Governor, GovernorTimelockControl)
     {
         super._executeOperations(proposalId, targets, values, calldatas, descriptionHash);
     }
 
     function _cancel(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
         internal
-        override(GovernorUpgradeable, GovernorTimelockControlUpgradeable)
+        override(Governor, GovernorTimelockControl)
         returns (uint256)
     {
         return super._cancel(targets, values, calldatas, descriptionHash);
@@ -121,7 +91,7 @@ contract LeeaGovernance is Initializable, GovernorUpgradeable, GovernorSettingsU
     function _executor()
         internal
         view
-        override(GovernorUpgradeable, GovernorTimelockControlUpgradeable)
+        override(Governor, GovernorTimelockControl)
         returns (address)
     {
         return super._executor();
